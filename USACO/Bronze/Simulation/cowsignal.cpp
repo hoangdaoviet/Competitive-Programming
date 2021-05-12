67955781e016290a2cfc181aa85720be2aaffb64
hoangdaoviet <77570106+hoangdaoviet@users.noreply.github.com>
2021-05-12 10:46:41 +0700

    Update cowsignal.cpp

--- USACO/Bronze/Simulation/cowsignal.cpp
+++ USACO/Bronze/Simulation/cowsignal.cpp
@@ -1,21 +1,21 @@
-#include <iostream>
-#include <cstdio>
+#include <bits/stdc++.h>
 using namespace std;
-int N,M,K;
 int main()
 {
-    freopen("cowsignal.in","r",stdin);
-    freopen("cowsignal.out","w",stdout);
-    cin >> M >> N >> K;
-    for (int i=0; i<M; i++){
-		string signal; cin >> signal;
-		for(int it = 0; it < K; it++){
-			for(int j = 0; j < N; j++){
-				for(int k = 0; k < K; k++){
-					cout << signal[j];
-				}
-			}
-			cout << endl;
-		}
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    ifstream cin ("cowsignal.in");
+    ofstream cout ("cowsignal.out");
+    int m,n,k;
+    cin >> m >> n >> k;
+    for (int i=0; i<m; i++){
+        string s; cin >> s;
+        for (int u=0; u<k; u++){
+            for (int j=0; j<n; j++){
+                for (int t=0; t<k; t++){
+                    cout << s[j];
+                }
+            }
+            cout << endl;
+        }
     }
 }
